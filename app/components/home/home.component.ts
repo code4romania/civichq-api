c9c910cece21e4eb0553352b8674bd0dea90f931
Alex Albu <alexp.albu@gmail.com>
2016-08-19 23:23:54 +0200

    Home page - bug fix
    Bug fix: Only apps are sorted, the categories remain unchanged; some
    code refactoring

--- app/components/home/home.component.ts
+++ app/components/home/home.component.ts
@@ -12,52 +12,63 @@ import { ListAppModel } from './list-app.model';
 export class HomeComponent implements OnInit {
 
   categories: CategoryModel[];
+  categoriesForApps: CategoryModel[];
+
   apps: ListAppModel[];
 
+
   private selectedCategory: CategoryModel;
-  
+
 
   constructor(private homeService: HomeService)
   { }
 
   ngOnInit() {
     this.homeService.getCategories()
-      .then(c => this.categories = c)
+      .then(c => { this.categories = c; this.categoriesForApps = c; })
       .catch(err => console.log(err));
 
     this.homeService.getTopChildrenForCategories()
       .then(a => this.apps = a)
       .catch(err => console.log(err));
 
-    
+
   }
 
   onSelectCategory(cat) {
     this.selectedCategory = cat;
 
-    this.sortCategories();
+    this.sortCategoriesForApps();
   }
 
-  private sortCategories() {
+  private sortCategoriesForApps() {
     if (this.selectedCategory) {
-      let newOrder: CategoryModel[] = [];
-      newOrder[0] = this.selectedCategory;
 
-      let restOfItems: CategoryModel[];
-      this.categories = this.categories.sort((o1, o2) => {
-        if (o1.Ordinal > o2.Ordinal) {
-          return 1;
-        }
-        if (o1.Ordinal < o2.Ordinal) {
-          return -1;
-        }
-        return 0;
-      });
-      restOfItems = this.categories.filter(x => x.Id != this.selectedCategory.Id);
+      let firstCategory = this.GetSelectedCategoryAsArray();
 
-      this.categories = newOrder.concat(restOfItems);
+      let restOfItems = this.GetCategoriesExceptSelected();
+
+      this.categoriesForApps = firstCategory.concat(restOfItems);
 
     }
+
+  }
+
+  private GetSelectedCategoryAsArray() {
+    let arr: CategoryModel[] = [];
+    arr[0] = this.selectedCategory;
+
+    return arr;
+  }
+
+
+  private GetCategoriesExceptSelected() {
+
+    let restOfItems: CategoryModel[];
+    this.categoriesForApps = this.categories;
+    restOfItems = this.categoriesForApps.filter(x => x.Id != this.selectedCategory.Id);
+
+    return restOfItems;
 
   }
 }
