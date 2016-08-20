f6fa12812b49fc80e983c99c3b9531f36970d3b4
Puiu <matrixpuiu@yahoo.com>
2016-08-20 23:05:42 +0200

    Added app profile service with mock data

--- app/components/app-profile/app-profile.component.ts
+++ app/components/app-profile/app-profile.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { AppProfileModel } from './../../shared/models/app-profile.model';
+import { Component, Input, OnInit } from '@angular/core';
+import { AppProfileService } from './app-profile.service';
+import { ActivatedRoute, Params } from '@angular/router';
+
 
 @Component({
     selector: 'app-profile',
-    templateUrl: 'app/components/app-profile/app-profile.component.html'
+    templateUrl: 'app/components/app-profile/app-profile.component.html',
+    providers: [AppProfileService]
 })
 
-export class AppProfileComponent{
+export class AppProfileComponent implements OnInit {
+
+    app: AppProfileModel;
+
+    constructor(private appProfileService: AppProfileService,
+        private route: ActivatedRoute) {
+
+    }
+
+    ngOnInit(): void {
+        this.route.params.forEach(
+            (params: Params) => {
+                let id = +params['id'];
+
+                this.appProfileService.getAppDetails(id)
+                    .then(a => this.app = a)
+                    .catch(err => console.log(err));
+            }
+        );
+        
+    }
 
 }
--- app/components/home/category.model.ts
+++
@@ -1,7 +0,0 @@
-
-export class CategoryModel
-{
-    Id: number;
-    CatName: string;
-    Ordinal: number;
-}
