cd711e5bf608f2e599ddf0f12f1dd7212c1c8368
Alex Albu <alexp.albu@gmail.com>
2016-09-18 00:30:37 +0200

    Refactoring
    Using same base select query for App profile API and Approve app API

--- api/app-profile-api.js
+++ api/app-profile-api.js
@@ -1,29 +1,9 @@
-function AppProfileApi() {
+var BaseAppQuery = require('./base-app-query');
 
-    this.baseAppQuery = 'SELECT a.id as \'appdetail.id\',' +
-            'a.AppName as \'appdetail.name\',' +
-            'a.website as \'appdetail.website\',' +
-            'a.facebook as \'appdetail.facebook\',' + 
-            'a.creationdate as \'appdetail.creationdate\',' +
-            'a.Logo as \'appdetail.logoname\',' +
-            'a.description as \'appdetail.description\',' +
-            'c.Id as \'appdetail.categoryid\',' +
-            'c.CatName as \'appdetail.categoryname\',' +
-            'a.tags as \'appdetail.hashtags\',' +
-            'a.github as \'appdetail.github\',' +
-            'n.NgoName as \'ngodetail.name\',' +
-            'n.phone as \'ngodetail.phone\',' +
-            'n.email as \'ngodetail.email\',' +
-            'n.facebook as \'ngodetail.facebook\',' +
-            'n.googleplus as \'ngodetail.googleplus\',' +
-            'n.linkedin as \'ngodetail.linkedin\',' +
-            'n.twitter as \'ngodetail.twitter\',' +
-            'n.instagram as \'ngodetail.instagram\',' +
-            'n.description as \'ngodetail.description\',' +
-            'n.logo as \'ngodetail.logoname\'' +
-            'FROM Apps a INNER JOIN Categories c on a.CategoryId = c.Id ' +
-            'INNER JOIN Ngos n on a.NgoId = n.Id '; 
-    
+function AppProfileApi() {
+    var bq = new BaseAppQuery();
+    this.baseAppQuery = bq.GetBaseAppQuery();
+
     this.baseWhere = ' WHERE a.IsApproved = 1 AND a.Id = :id';
 
 }
@@ -31,8 +11,9 @@ function AppProfileApi() {
 AppProfileApi.prototype = {
 
     GetAppProfile: function (res, seq, id) {
+
         var appQuery = this.baseAppQuery + this.baseWhere;
-       
+
         var p1 = seq.query(appQuery,
             {
                 replacements: { id: id },
@@ -49,12 +30,12 @@ AppProfileApi.prototype = {
     GetMasterProfile: function (res, seq) {
         var query = this.baseAppQuery + ' WHERE a.IsMaster = 1; ';
 
-            seq.query(query, {
-                type: seq.QueryTypes.SELECT,
-                nest: true
-            }).then(function(a){
-                res.json(a[0]);
-            });
+        seq.query(query, {
+            type: seq.QueryTypes.SELECT,
+            nest: true
+        }).then(function (a) {
+            res.json(a[0]);
+        });
 
     }
 
