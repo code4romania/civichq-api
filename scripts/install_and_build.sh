7f08b8e2375c21479abbeeb07277034164dd73fe
Vic <cvctor@gmail.com>
2016-11-10 18:34:19 +0200

    add: nvm install steps

--- scripts/install_and_build.sh
+++ scripts/install_and_build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-cd /www/civichq-api
-npm install
-# npm run postinstall
-# npm run build
 
-pkill -9 PM2 && pkill -9 node # sorry pm2, you have to really restart
-pm2 startOrRestart ecosystem.json5
+if ! [ -x "$(command -v nvm)" ]; then
+  echo 'nvm is not installed.' >&2
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+fi
+
+. $HOME/.nvm/nvm.sh
+
+nvm install node
+
+npm install
+npm install -g pm2
+
+pm2 startOrRestart ecosystem-my.json5
+pm2 start api/server.js
+
