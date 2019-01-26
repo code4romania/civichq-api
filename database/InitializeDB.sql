58a53e3e3425dc80efcc0f38246369ec23579161
Antoniu Ienciu <tony_ienciu@yahoo.com>
2019-01-26 18:03:15 +0200

    Update InitializeDB.sql

--- database/InitializeDB.sql
+++ database/InitializeDB.sql
@@ -3,13 +3,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
--- -----------------------------------------------------
--- Schema civichq
--- -----------------------------------------------------
-
--- -----------------------------------------------------
 -- Schema civichq
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `civichq` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
